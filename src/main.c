eb6454194cea2ed89f7b99023e9fc2e649d51f2d
Nikita Saveliev <nikita.saveli3v@yandex.ru>
2023-12-20 12:30:10 +0300

    init
    initial commit (led+adv)

---
+++ src/main.c
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file Sample app to demonstrate PWM.
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+
+#define MIN_PERIOD PWM_SEC(1U) / 128U
+#define MAX_PERIOD PWM_SEC(1U)
+
+
+static bt_addr_le_t custom_addr = {
+    .type = BT_ADDR_LE_PUBLIC,
+    .a.val = {0x01, 0x00, 0x00, 0x00, 0x00, 0x10},
+};
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
+	BT_DATA_BYTES(BT_DATA_SVC_DATA16,
+		      0xaa, 0xfe, /* Eddystone UUID */
+		      0x10, /* Eddystone-URL frame type */
+		      0x00, /* Calibrated Tx power at 0m */
+		      0x00, /* URL Scheme Prefix http://www. */
+		      'z', 'e', 'p', 'h', 'y', 'r',
+		      'p', 'r', 'o', 'j', 'e', 'c', 't',
+		      0x08) /* .org */
+};
+
+/* Set Scan Response data */
+static const struct bt_data sd[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN), //fixed mac-address
+};
+
+static void bt_ready(int err)
+{
+	size_t count = 1;
+
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+	
+	/* Start advertising */
+	//bt_id_create(&custom_addr, NULL); 
+	err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+	/* For connectable advertising you would use
+	 * bt_le_oob_get_local().  For non-connectable non-identity
+	 * advertising an non-resolvable private address is used;
+	 * there is no API to retrieve that.
+	 */
+
+}
+
+
+
+int main(void)
+{
+	static const struct device *const clock0 = DEVICE_DT_GET_ONE(nordic_nrf_clock);
+(void) clock_control_on(clock0, CLOCK_CONTROL_NRF_SUBSYS_HF);
+	uint32_t max_period;
+	uint32_t period;
+	uint8_t dir = 0U;
+	int ret;
+
+	printk("PWM-based blinky\n");
+
+	if (!pwm_is_ready_dt(&pwm_led0)) {
+		printk("Error: PWM device %s is not ready\n",
+		       pwm_led0.dev->name);
+		return 0;
+	}
+
+	/*
+	 * In case the default MAX_PERIOD value cannot be set for
+	 * some PWM hardware, decrease its value until it can.
+	 *
+	 * Keep its value at least MIN_PERIOD * 4 to make sure
+	 * the sample changes frequency at least once.
+	 */
+	printk("Calibrating for channel %d...\n", PWM0_CH_NUM);
+	max_period = MAX_PERIOD;
+	while (pwm_set_dt(&pwm_led0, max_period, max_period / 2U)) {
+		max_period /= 2U;
+		if (max_period < (4U * MIN_PERIOD)) {
+			printk("Error: PWM device "
+			       "does not support a period at least %lu\n",
+			       4U * MIN_PERIOD);
+			return 0;
+		}
+	}
+
+	printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
+	       max_period, MIN_PERIOD);
+
+	period = max_period;
+	
+		ret = pwm_set_dt(&pwm_led0, period, period / 2U);
+		if (ret) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return 0;
+		}
+
+		period = dir ? (period * 2U) : (period / 2U);
+		if (period > max_period) {
+			period = max_period / 2U;
+			dir = 0U;
+		} else if (period < MIN_PERIOD) {
+			period = MIN_PERIOD * 2U;
+			dir = 1U;
+		}
+
+		k_sleep(K_SECONDS(4U));
+
+		int err;	
+		printk("Starting Beacon Demo\n");
+
+	/* Initialize the Bluetooth Subsystem */
+	
+	//bt_id_create(&custom_addr, NULL); 
+	err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
+//	while(1){}
+	return 0;
+	
+}
