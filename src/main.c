2f3bf9ba54613021d52a5f5d4827c8f1d191ff5c
Nick <nikita.saveli3v@yandex.ru>
2023-12-27 10:50:47 +0300

    led indi
    led indi added

--- src/main.c
+++ src/main.c
@@ -1,20 +1,4 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
-/**
- * @file Sample app to demonstrate PWM.
- */
-
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/pwm.h>
-#include <zephyr/drivers/clock_control.h>
-#include <zephyr/drivers/clock_control/nrf_clock_control.h>
 
 #include <zephyr/types.h>
 #include <stddef.h>
@@ -23,16 +7,25 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
-#include <zephyr/drivers/clock_control.h>
-#include <zephyr/drivers/clock_control/nrf_clock_control.h>
+
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SLEEP_TIME_MS   1
+
+#define LED1_NODE DT_ALIAS(led1) //green
+#define LED2_NODE DT_ALIAS(led2) //red
+#define LED3_NODE DT_ALIAS(led3) //blue
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
-static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec blue = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
-#define MIN_PERIOD PWM_SEC(1U) / 128U
-#define MAX_PERIOD PWM_SEC(1U)
+
 
 bool deviceFound = false;
 
@@ -103,60 +96,48 @@ static void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
 
 int main(void)
 {
+
+
+//
+	int ret;
+
+	if (!gpio_is_ready_dt(&red)) {
+		return 0;
+	}
+
+	if (!gpio_is_ready_dt(&green)) {
+		return 0;
+	}
+
+	if (!gpio_is_ready_dt(&blue)) {
+		return 0;
+	}
+	ret = gpio_pin_configure_dt(&red, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&green, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&blue, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+
+		
+//
+
 	set_public_addr();
 	uint32_t max_period;
 	uint32_t period;
 	uint8_t dir = 0U;
-	int ret;
 
-	printk("PWM-based blinky\n");
 
-	if (!pwm_is_ready_dt(&pwm_led0)) {
-		printk("Error: PWM device %s is not ready\n",
-		       pwm_led0.dev->name);
-		return 0;
-	}
 
-	/*
-	 * In case the default MAX_PERIOD value cannot be set for
-	 * some PWM hardware, decrease its value until it can.
-	 *
-	 * Keep its value at least MIN_PERIOD * 4 to make sure
-	 * the sample changes frequency at least once.
-	 */
-	printk("Calibrating for channel %d...\n", PWM0_CH_NUM);
-	max_period = MAX_PERIOD;
-	while (pwm_set_dt(&pwm_led0, max_period, max_period / 2U)) {
-		max_period /= 2U;
-		if (max_period < (4U * MIN_PERIOD)) {
-			printk("Error: PWM device "
-			       "does not support a period at least %lu\n",
-			       4U * MIN_PERIOD);
-			return 0;
-		}
-	}
-
-	printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
-	       max_period, MIN_PERIOD);
-
-	period = max_period;
-	
-		ret = pwm_set_dt(&pwm_led0, period, period / 2U);
-		if (ret) {
-			printk("Error %d: failed to set pulse width\n", ret);
-			return 0;
-		}
-
-		period = dir ? (period * 2U) : (period / 2U);
-		if (period > max_period) {
-			period = max_period / 2U;
-			dir = 0U;
-		} else if (period < MIN_PERIOD) {
-			period = MIN_PERIOD * 2U;
-			dir = 1U;
-		}
-
-		k_sleep(K_SECONDS(4U));
 
 		int err;	
 		printk("Starting Beacon Demo\n");
@@ -165,27 +146,32 @@ int main(void)
 	
 	//bt_id_create(&custom_addr, NULL); 
 	struct bt_le_scan_param scan_param = {
-		.type       = BT_HCI_LE_SCAN_PASSIVE,
+		.type       = BT_HCI_LE_SCAN_ACTIVE,
 		.options    = BT_LE_SCAN_OPT_NONE,
 		.interval   = 0x0010,
 		.window     = 0x0010,
 	};
 	err = bt_enable(NULL); //bt_ready
-
 	err = bt_le_scan_start(&scan_param, scan_cb);
 	if (err) {
 		printk("Starting scanning failed (err %d)\n", err);
 		return 0;
 	}
-
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 	}
 	printk("Starting Scanner/Advertiser Demo\n");
-	
 	do {
+		
 		k_sleep(K_MSEC(400));
 		if(deviceFound){
+				//ret = pwm_set_dt(&pwm_led0, period, period/2);
+		//ret = gpio_pin_toggle_dt(&red);
+		gpio_pin_set(red.port,15,0);
+		
+		if (ret < 0) {
+			return 0;
+		}
 		/* Start advertising */
 		err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
 			      sd, ARRAY_SIZE(sd));
@@ -195,8 +181,13 @@ int main(void)
 		}
 
 		k_sleep(K_MSEC(400));
+		
+		}else {
+			gpio_pin_set(red.port,15,1);
 		}
 		err = bt_le_adv_stop();
+		deviceFound = false;
+		
 		if (err) {
 			printk("Advertising failed to stop (err %d)\n", err);
 			return 0;
